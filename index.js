5185f343b125b25fe67405796c60d0a5b0772ea3
Renee <156127959+reneexie01@users.noreply.github.com>
2024-10-09 06:52:05 +1100

    Set up the class contructors for LinkedList and Node

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('hello')
+// Class for Node
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// Class for LinkedList
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+}
+
+const family = new LinkedList();
+
+family.append('Logan');
+family.append('Tina');
+family.append('Leya');
+family.append('Remee');
+family.prepend('Start')
+
+console.log(family)
+
